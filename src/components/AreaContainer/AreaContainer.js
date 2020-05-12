92b03d7986a1c0a92a57897010b18ffedebde2e3
collinkallery <collin.kallery@colorado.edu>
2020-05-12 17:16:16 -0600

    Add in basic styling for area container, map over areas

--- src/components/AreaContainer/AreaContainer.js
+++ src/components/AreaContainer/AreaContainer.js
@@ -1,10 +1,30 @@
 import  React from 'react';
 import './AreaContainer.css';
+import Area from '../Area/Area';
 
-const AreaContainer = () => {
+const AreaContainer = ({areas}) => {
+
+  const areasToDisplay = areas.map(area => {
+    return (
+      <Area
+        area={area.area}
+        details={area.details}
+        id={area.id}
+        name={area.name}
+        location={area.location}
+        about={area.about}
+        region_code={area.region_code}
+        quick_search={area.quick_search}
+        listings={area.listings}
+      />
+    )
+  })
+
   return (
-    <p>Hi</p>
+    <section className="areas-container">
+      {areasToDisplay}
+    </section>
   )
 }
 
-export default AreaContainer 
+export default AreaContainer;
