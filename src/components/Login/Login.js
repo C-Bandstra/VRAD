7a76b9f0da7599730c6532f9d5c490157ed20956
collinkallery <collin.kallery@colorado.edu>
2020-05-13 15:35:59 -0600

    Pass down prop for checking form status, enabling login button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,43 @@
 import  React, {Component} from 'react';
 import './Login.css';
+import {Route, Link} from "react-router-dom";
+
+
 class Login extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       name: '',
       email: '',
-      purpose: ''
+      purpose: '',
     }
   }
 
   trackInput = (e) => {
     const keyword = e.target.id
     this.setState({[keyword]: e.target.value})
+    this.enableLoginButton();
   }
 
   trackPurpose = (e) => {
     let purpose = e.target.options[e.target.selectedIndex].value
     this.setState({purpose: purpose})
+    this.enableLoginButton();
   }
+
+  enableLoginButton = () => {
+    const state = this.state;
+    debugger;
+    if (state.name && state.email && state.purpose) {
+      this.props.checkForm(true);
+    } else {
+      this.props.checkForm(false);
+    }
+  }
+
+  // If any of the forms are empty / unselected, display an error message
+  // telling the user to complete the form.
+  // Else, display the areas page.
 
   //on button click, change page while passing in this.state values
 
@@ -37,7 +56,11 @@ class Login extends Component {
                 <option value="vacation">Vacation</option>
                 <option value="other">Other</option>
               </select>
-              <button className="btn login-btn">Login</button>
+              <button className="btn login-btn">
+                <Link to="Areas">
+                  Login
+                </Link>
+              </button>
             </div>
           </section>
         </section>
