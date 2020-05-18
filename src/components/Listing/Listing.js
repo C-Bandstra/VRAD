f126fbfddd99bd2bcb7f1079418acc85cda3ab0f
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-18 13:22:55 -0600

    Fix listing breaking change

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -4,14 +4,11 @@ import './Listing.css';
 const Listing = (props) => {
 
   const renderListingDetails = () => {
-    let detailsToRender = Object.keys(props.details).map(detail => {
       return (`
         Number of beds: ${props.details.beds}
         Number of baths: ${props.details.baths}
         Cost per night: ${props.details.cost_per_night}
         `);
-    })
-    return detailsToRender;
   }
 
   console.log('hi', renderListingDetails());
