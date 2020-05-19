d37b82c41fc4b372ed15a6a4133926b3c9ad6923
collinkallery <collin.kallery@colorado.edu>
2020-05-18 21:55:07 -0600

    Functional component for the navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './NavBar.css';
+import { Link } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <section className="navbar-container">
-      <h3 className="vrad-header">VRAD</h3>
-      <h2 className="page-header">Listings</h2>
+      <h3 className="vrad-header"><span className="v">V</span>RAD</h3>
+      <h2 className="page-header">{props.title}</h2>
+      <section className="user-about-container">
+        <p className="welcome-user">Welcome, {props.userInfo.name}!</p>
+        <Link className="logout-link" to="/">
+          <button className="logout-button">Sign Out</button>
+        </Link>
+      </section>
     </section>
   )
 }
+
+export default NavBar;
