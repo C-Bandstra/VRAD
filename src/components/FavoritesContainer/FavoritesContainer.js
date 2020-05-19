a262e0f1da027bd631ab6dbdb7696991e1d1ac98
collinkallery <collin.kallery@colorado.edu>
2020-05-19 14:20:11 -0600

    Render favorites container and map for favorites and ternary for no favorites message

--- src/components/FavoritesContainer/FavoritesContainer.js
+++ src/components/FavoritesContainer/FavoritesContainer.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './FavoritesContainer.css';
+import NavBar from '../NavBar/NavBar';
+import Listing from '../Listing/Listing';
 
-const FavoritesContainer = () => {
+const FavoritesContainer = ({userInfo, signOut}) => {
 
+  const favoritesToDisplay = () => {
+    let favorites = userInfo.favorites.map(favorite => {
+      return (
+        <Listing key={favorite.listing_id} {...favorite} />
+      )
+    })
+    return favorites;
+  }
+
+  return (
+    <section className="favorites-page">
+      <NavBar
+        title={`Favoritied ${userInfo.purpose} Listings`}
+        userInfo={userInfo}
+        signOut={signOut}
+      />
+      <section className="favorites-container">
+        {!userInfo.favorites ?
+          <h3 className="no-favorites-message">You don't have any favorites yet! Head back to the listings and choose a favorite.</h3> :
+          favoritesToDisplay
+        }
+      </section>
+    </section>
+  )
 }
 
 export default FavoritesContainer;
