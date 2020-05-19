6704a95e877bc5b9d7d10ec21b8f8714d3cf0795
collinkallery <collin.kallery@colorado.edu>
2020-05-19 16:20:01 -0600

    Display favorites method

--- src/components/FavoritesContainer/FavoritesContainer.js
+++ src/components/FavoritesContainer/FavoritesContainer.js
@@ -11,20 +11,21 @@ const FavoritesContainer = ({userInfo, signOut}) => {
         <Listing key={favorite.listing_id} {...favorite} />
       )
     })
+    console.log('ur mothers butt', favorites);
     return favorites;
   }
 
   return (
     <section className="favorites-page">
       <NavBar
-        title={`Favoritied ${userInfo.purpose} Listings`}
+        title={`Your Favorite ${userInfo.purpose} Listings`}
         userInfo={userInfo}
         signOut={signOut}
       />
       <section className="favorites-container">
-        {!userInfo.favorites ?
+        {userInfo.favorites.length < 1 ?
           <h3 className="no-favorites-message">You don't have any favorites yet! Head back to the listings and choose a favorite.</h3> :
-          favoritesToDisplay
+          favoritesToDisplay()
         }
       </section>
     </section>
